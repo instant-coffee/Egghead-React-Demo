4bda59f4387972177ba3cc9bdaf31a2d465140fe
Lachlan MacKenzie <lachlanrm@gmail.com>
2017-01-18 13:02:22 -0800

    Binding to state

--- src/App.js
+++ src/App.js
@@ -1,5 +1,34 @@
 import React from 'react';
 
-const App = () => <h1>HI THERE</h1>
+class App extends React.Component {
 
+  constructor() {
+    super();
+    this.state = {
+      text: 'This is state text'
+    }
+  }
+
+  update (e) {this.setState({text: e.target.value})}
+  render() {
+    return (
+      
+      <div>
+        <input type="text"
+          onChange={this.update.bind(this)}
+        />  
+        <h1>{this.state.text}</h1>
+      </div>
+    )
+  }
+
+}
+
+App.propTypes = {
+  text: React.PropTypes.string
+}
+
+App.defaultProps = {
+  text: "blah"
+}
 export default App
